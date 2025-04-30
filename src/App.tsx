d5692bd1bf6977470cca625a1d4994a920ce31d1
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-04-30 17:39:41 +0530

    Implementing theme based color for elements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,76 @@
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
 import React from 'react';
 
+let isDarkMode = false;
+
+const lightTheme = {
+  background: 'rgba(255, 223, 223, 1)',
+  text: '#333333',
+  titleBar: 'rgba(255, 191, 191, 1)', // Light Red
+  buttonBackground: 'rgba(255, 127, 127, 1)',
+  statusBarBackground: 'rgba(255, 191, 191, 1)',
+  statusBarStyle: 'dark-content',
+};
+
+const darkTheme = {
+  background: 'rgba(127, 0, 0, 1)',
+  text: '#FFFFFF',
+  titleBar: 'rgba(255, 31, 31, 1)', // Dark Red
+  buttonBackground: 'rgba(255, 31, 31, 1)',
+  statusBarBackground: 'rgba(255, 31, 31, 1)',
+  statusBarStyle: 'light-content',
+};
+
 function App() {
+  const colorScheme = useColorScheme(); // 'light' or 'dark'
+
+  isDarkMode = colorScheme === 'dark';
+
+  const statusBarStyle = isDarkMode ? 'light-content' : 'dark-content';
+
+  const currentTheme = isDarkMode ? darkTheme : lightTheme;
+
   return (
-    <SafeAreaView style={styles.mainContainer}>
-      <View style={styles.titleBar}>
-        <Text style={styles.text}>Title Bar</Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.text}>Hello World</Text>
-        <Text style={styles.text}>Hello World</Text>
-        <Text style={styles.text}>Hello World</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <StatusBar
+        backgroundColor={currentTheme.statusBarBackground}
+        barStyle={statusBarStyle}
+      />
+      <SafeAreaView
+        style={[
+          styles.mainContainer,
+          {backgroundColor: currentTheme.background},
+        ]}>
+        <View
+          style={[styles.titleBar, {backgroundColor: currentTheme.titleBar}]}>
+          <Text style={[styles.text, {color: currentTheme.text}]}>
+            Title Bar
+          </Text>
+        </View>
+        <View
+          style={[
+            styles.sectionContainer,
+            {backgroundColor: currentTheme.background},
+          ]}>
+          <Text style={[styles.text, {color: currentTheme.text}]}>
+            Hello World
+          </Text>
+          <Text style={[styles.text, {color: currentTheme.text}]}>
+            Hello World
+          </Text>
+          <Text style={[styles.text, {color: currentTheme.text}]}>
+            Hello World
+          </Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
@@ -21,10 +79,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   titleBar: {
-    backgroundColor: 'rgba(255, 191, 191, 1)',
     padding: 5,
     width: '100%',
     alignItems: 'center',
@@ -33,7 +89,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     paddingVertical: 16,
     paddingHorizontal: 24,
-    backgroundColor: 'rgba(255, 191, 191, 1)',
     flex: 1,
     justifyContent: 'space-evenly',
   },
@@ -42,7 +97,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
     color: '#333333',
-    backgroundColor: 'rgba(255, 127, 127, 1)',
   },
 });
 
